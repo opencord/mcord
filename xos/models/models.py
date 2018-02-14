178fca886e58cc68293502261568d4795653ff9d
Matteo Scandolo <teo@opennetworking.org>
2018-02-14 15:21:29 -0800

    [CORD-2550] Adding name and improving status
    Change-Id: I4680d7427ee833094b2ddd0a290945b2052d1087
    (cherry picked from commit 95f585e86af3f4480b6bfcee958a14fd041bab5a)

--- xos/models/models.py
+++ xos/models/models.py
@@ -23,6 +23,10 @@ class MCordSubscriberInstance(MCordSubscriberInstance_decl):
         except IndexError:
             raise XOSValidationError("Service MCORD cannot be found, please make sure that the model exists.")
 
+        # if we don't have a name, use the IMSI number has a name
+        if not self.name:
+            self.name = self.imsi_number
+
         # prevent IMSI duplicate
         try:
             instance_with_same_imsi = MCordSubscriberInstance.objects.get(imsi_number=self.imsi_number)
@@ -35,5 +39,8 @@ class MCordSubscriberInstance(MCordSubscriberInstance_decl):
         if self.is_new and not self.created_by:
             # NOTE if created_by is null it has been created by XOS
             self.created_by = "XOS"
+
+        self.backend_code = 0
+        self.backend_status = "In Progress"
 
         super(MCordSubscriberInstance, self).save(*args, **kwargs)
