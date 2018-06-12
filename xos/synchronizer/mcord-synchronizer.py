10ca68a21e74e71a59c147c9afbd6c2fcd9759e3
Matteo Scandolo <teo@opennetworking.org>
2018-06-12 13:33:12 -0700

    [CORD-3022] Splitting config
    Change-Id: I1e802b052a6e5f59e17215864ec18b9acb945a14

--- xos/synchronizer/mcord-synchronizer.py
+++ xos/synchronizer/mcord-synchronizer.py
@@ -23,8 +23,13 @@ import os
 import sys
 from xosconfig import Config
 
-config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/mcord_config.yaml')
-Config.init(config_file, 'synchronizer-config-schema.yaml')
+base_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/config.yaml')
+mounted_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/mounted_config.yaml')
+
+if os.path.isfile(mounted_config_file):
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml', mounted_config_file)
+else:
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml')
 
 observer_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"../../synchronizers/new_base")
 sys.path.append(observer_path)
